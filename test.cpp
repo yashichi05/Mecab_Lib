f3d56d26ef7aceedd77f2bb5a7a15f900ec00e34
yashichi05 <39804690+yashichi05@users.noreply.github.com>
2021-04-10 07:29:52 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,32 +10,30 @@ struct Extension
     std::wstring name;
     wchar_t *(*callback)(wchar_t *, const InfoForExtension *);
 };
+
+std::array<InfoForExtension, 10> info_ary{{
+    {"current select", 1},
+    {"text number", 1},
+    {"process id", 2},
+    {"hook address", 3},
+    {"text handle", 4},
+    {"text name", 5},
+    {"void (*AddSentence)(int64_t number, const wchar_t* sentence)", 6},
+    {"void (*AddText)(int64_t number, const wchar_t* text)", 7},
+    {"DWORD (*GetSelectedProcessId)()", 8},
+    {nullptr, 0} // nullptr marks end of info array
+}};
+InfoForExtension *info_arg = info_ary.data();
 int main(int argc, char **argv)
 {
-    concurrency::reader_writer_lock extenMutex;
     HMODULE module = LoadLibraryW(L"visual Extension.dll");
     auto callback = (decltype(Extension::callback))GetProcAddress(module, "OnNewSentence");
-    std::scoped_lock writeLock(extenMutex);
-    std::array<InfoForExtension, 10> b_ary{{
-        {"current select", 1},
-        {"text number", 1},
-        {"process id", 2},
-        {"hook address", 3},
-        {"text handle", 4},
-        {"text name", 5},
-        {"void (*AddSentence)(int64_t number, const wchar_t* sentence)", 6},
-        {"void (*AddText)(int64_t number, const wchar_t* text)", 7},
-        {"DWORD (*GetSelectedProcessId)()", 8},
-        {nullptr, 0} // nullptr marks end of info array
-    }};
-    InfoForExtension *b = b_ary.data();
-    // b->name = "vs";
-    callback(L"aaaa999999999999999999999999999999999", b);
+
+    callback(L"aaaa999999999999999999999999999999999", info_arg);
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    // std::cin.get();
 }
