1539bee83bcae8b9e607e76a5794a4fd01db5812
yashichi05 <39804690+yashichi05@users.noreply.github.com>
2021-04-09 22:31:46 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <Windows.h>
+#include "Extension.h"
 
-int main(int argc, char **argv){
-    LoadLibraryW(L"mecabWindow.dll");
-    return 0;
+
+struct Extension
+{
+    std::wstring name;
+    wchar_t *(*callback)(std::wstring&, const InfoForExtension *);
+};
+int main(int argc, char **argv)
+{
+    HMODULE module = LoadLibraryW(L"vExten.dll");
+    auto callback = (decltype(Extension::callback))GetProcAddress(module, "OnNewSentence");
+    InfoForExtension *b;
+    b->value = 0;
+    b->name = "vs";
+    std::wstring a = L"aaaa";
+    callback(a,b);
 }
